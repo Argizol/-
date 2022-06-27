62788bac9667c4e3e942d10a2760c002bafeb34c
Dmitriy <argizol@mail.ru>
2022-06-28 00:00:18 +0300

    Первая итерация задания

---
+++ Lesson2/Lesson2/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    public class BankAccount
+    {
+        private string _accountNumber;
+        private decimal _balance;
+        public enum _AccountType
+        {
+            Debit,
+            Credit,
+            Saving
+        }
+        private _AccountType _accountType;
+
+        public string AccountNumber
+        { 
+            get { return _accountNumber; } 
+            set { _accountNumber = value; }
+        }
+
+        public decimal Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+
+        public _AccountType AccountType
+        { get { return _accountType;}
+          set { _accountType = value; }
+        }
+    }
+}
--- Lesson2/Lesson2/Program.cs
+++ Lesson2/Lesson2/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Lesson2;
+
+BankAccount bankAccount1 = new BankAccount();
+bankAccount1.AccountNumber = "12313123";
+bankAccount1.Balance = 123123.432M;
+bankAccount1.AccountType = BankAccount._AccountType.Debit;
+
+Console.Write($"{bankAccount1.AccountNumber}\n{bankAccount1.Balance}\n{bankAccount1.AccountType}");
+Console.ReadLine();
