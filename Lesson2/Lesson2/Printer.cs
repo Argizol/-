88789d8a18d0a4fe85e98e1f7b54e74d8471cbb5
Dmitriy <argizol@mail.ru>
2022-07-03 18:27:49 +0300

    ДЗ 3 ч.1

---
+++ Lesson2/Lesson2/Printer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    public static class Printer
+    {
+        public static void PrintNoMoney()
+        {
+            Console.WriteLine("На счете недостаточно средств \n");
+        }
+    }
+}
