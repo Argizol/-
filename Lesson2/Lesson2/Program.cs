628c541ff8ba279028cb382ee70da78b0240b735
Dmitriy <argizol@mail.ru>
2022-07-03 18:58:22 +0300

    ДЗ 3 ч.2

--- Lesson2/Lesson2/Program.cs
+++ Lesson2/Lesson2/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Lesson2;
+using System.Text;
 
 BankAccount bankAccount1 = new BankAccount(1, 500.00M, BankAccount._AccountType.Debit);
 BankAccount bankAccount2 = new BankAccount(1, 100.00M, BankAccount._AccountType.Saving);
@@ -7,4 +8,9 @@ Console.WriteLine($"{bankAccount1.Balance} {bankAccount2.Balance} \n");
 bankAccount2.TransferMoney(ref bankAccount1, 6000);
 bankAccount2.TransferMoney(ref bankAccount1, 200);
 Console.WriteLine($"{bankAccount1.Balance} {bankAccount2.Balance} \n");
+
+Console.WriteLine("Введите слово");
+string word = Console.ReadLine();
+Console.WriteLine(Reverse.ReverseWord(word));
+
 Console.ReadLine();
