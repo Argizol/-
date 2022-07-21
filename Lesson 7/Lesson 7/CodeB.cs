dc7d796195a2ba82596389a9dd964c12299ff9cb
Dmitriy <argizol@mail.ru>
2022-07-21 21:38:54 +0300

    ДЗ 7 кодируем со смещением на 1 букву вперед

---
+++ Lesson 7/Lesson 7/CodeB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7
+{
+    // Класс шифрует строку, выполняя замену каждой буквы, стоящей в алфавите на i-й позиции, на букву того же регистра,
+    // расположенную в алфавите на i-й позиции с конца алфавита.
+    // (Например, буква В заменяется на букву Э)
+
+    internal class CodeB : ICoder
+    {
+        public string Decoder(string str, char[] alfabet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Encoder(string str, char[] alfabet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lesson 7/Lesson 7/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Lesson_7;
+
+char[] alfabet = { 
+    'а', 'б', 'в', 'г', 'д',
+    'е', 'ё', 'ж', 'з', 'и',
+    'й', 'к', 'л', 'м', 'н',
+    'о', 'п', 'р', 'с', 'т',
+    'у', 'ф', 'х', 'ц', 'ч',
+    'ш', 'щ', 'ъ', 'ы', 'ь', 
+    'э', 'ю', 'я' };
+
+Console.WriteLine("Введите строку на русском языке");
+string str = Console.ReadLine().ToLower();
+
+CodeA codeA = new();
+
+if (str == string.Empty)
+{
+    do
+    {
+        Console.WriteLine("Вы не ввели строку на русском языке");
+        Console.WriteLine("Введите строку на русском языке");
+        str = Console.ReadLine();
+    }
+    while (str == string.Empty);
+}
+
+str = codeA.Encoder(str, alfabet);
+Console.WriteLine(str);
+str = codeA.Decoder(str, alfabet);
+Console.WriteLine(str);
+
+Console.ReadLine();
